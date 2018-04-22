3f2bf558945741da61e8a30cfcc2f06242aee170
Basava Hadimani <basuhadimani94@gmail.com>
2018-04-22 23:42:14 +0530

    Changed

---
+++ component/ts/head.jsx
@@ -0,0 +1,14 @@
+import { h, render, Component } from 'preact';
+
+export default class Head extends Component{
+
+ 	render(){
+ 	return(
+ 		<div id="head" class="row">
+          <img class="w3-border w3-padding logo img-responsive" src="https://teja10.kuikr.com/public/images/dist/PNG/Quikr-Logo.png" alt="Quikr logo" title="Quikr logo" />
+        </div>
+ 	);
+ 	}
+
+ }
+
